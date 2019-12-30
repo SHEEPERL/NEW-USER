6ffc9d400c98ab7e61320d2237db7f058d25fd24
Kinsa <Kinsa@DESKTOP-933U25U>
2019-12-30 22:20:51 +0800

    第九章

---
+++ 第十一章/src/TestCircleRectangle.java
@@ -0,0 +1,17 @@
+//11-4
+public class TestCircleRectangle {
+	public static void main(String[] args) {
+		CircleFromSimpleGeometricObject = new CircleFromSimpleGeometricObject(1);
+		System.out.println("A circle " + circle.toString());
+		System.out.println("Te color is " + circle.getColor());
+		System.out.println("The radius is " + circle.getRadius());
+		System.out.println("The area is " + circle.getArea());
+		System.out.println("The diameter is " + circle.getDiameter());
+		
+		RectangleFromSimpleGeometricObject = new RectangleFromSimpleGeometricObject(2,4);
+		System.out.println("\nA rectangle " + rectangle.toString());
+		System.out.println("The area is " + rectangle.getArea());
+		System.out.println("The primeter is " + ractangle.getPrimeter());
+		rectangle.getPerimeter());
+	}
+}
